f1f6af04bbde11bee240f5b95a20816533d4c30a
Harry Tran <harry.tran@dssolution.jp>
2025-04-24 18:59:51 +0700

    remove unuse file

--- microservice_2/access-control-service/src/main/java/com/example/accesscontrolservice/repository/AreaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.accesscontrolservice.repository;
-
-import com.example.accesscontrolservice.model.Area;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AreaRepository extends JpaRepository<Area, String> {
-}
